45f8a54f81bfe545ae94e5c66fd88a99026d9d4e
Sanjin Dumančić <sdumancic@sit.pbz.hr>
2019-01-03 13:02:19 +0100

    part 1 finished

---
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -0,0 +1,58 @@
+package io.agileintelligence.ppmtool.services;
+
+import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
+import io.agileintelligence.ppmtool.repositories.ProjectRepository;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }
+        catch (Exception ex){
+            if (ex.getCause().getClass() == org.hibernate.exception.ConstraintViolationException.class){
+                ConstraintViolationException cause = (ConstraintViolationException)ex.getCause();
+                String errMessage = cause.getSQLException().getMessage();
+                if (errMessage.toUpperCase().startsWith("UNIQUE INDEX OR PRIMARY KEY VIOLATION") && errMessage.toUpperCase().contains("PROJECT(PROJECT_IDENTIFIER)"))
+                    throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase() + " already exists");
+            }
+            throw ex;
+        }
+
+    }
+
+    public Project findProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+
+        if (project == null){
+            throw new ProjectIdException("Project ID " + projectId.toUpperCase() + " does not exist");
+        }
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+
+        if (project == null){
+            throw new ProjectIdException("Cannot delete project with Project ID " + projectId.toUpperCase() + " because it does not exist");
+        }
+
+        projectRepository.delete(project);
+    }
+
+
+}
+
---
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -0,0 +1,68 @@
+package io.agileintelligence.ppmtool.web;
+
+import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.services.ProjectService;
+import io.agileintelligence.ppmtool.services.ValidationErrorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/project")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private ValidationErrorService validationErrorService;
+
+//    @PostMapping("")
+//    public ResponseEntity<Project> saveProject(@RequestBody Project project, UriComponentsBuilder ucb){
+//        HttpHeaders headers = new HttpHeaders();
+//        Project project = projectService.save(project);
+//        URI locationUri = ucb.path("/projects/")
+//                        .path(String.valueOf("12345"))
+//                        .build()
+//                        .toUri();
+//        headers.setLocation(locationUri);
+//        ResponseEntity<Project> responseEntity = new ResponseEntity<Project>(
+//                        project, headers, HttpStatus.CREATED)
+//        return responseEntity;
+//    }
+
+    @PostMapping
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        ResponseEntity<?> errorMap = validationErrorService.mapValidationService(result);
+        if (errorMap != null)
+            return errorMap;
+
+        Project project1 = projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project1,HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId){
+        Project project = projectService.findProjectByIdentifier(projectId);
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjects(){
+        return projectService.findAllProjects();
+    }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId){
+        projectService.deleteProjectByIdentifier(projectId);
+        return new ResponseEntity<String>("Project with id " + projectId + " was deleted",HttpStatus.OK);
+    }
+
+}
